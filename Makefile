b8b61c2d38be8606d7d220a6a9b67a95d9c4d2da
Oleg Tarasenko <oltarasenko@gmail.com>
2017-07-07 15:48:39 +0300

    Shaped the application architecture

--- Makefile
+++ Makefile
@@ -2,4 +2,8 @@ PROJECT = erlang-node-discovery
 PROJECT_DESCRIPTION = New project
 PROJECT_VERSION = 0.1.0
 
+SHELL_OPTS = -config config/devel.config
+
+devel: all shell
+
 include erlang.mk
--- src/erlang_node_discovery_sup.erl
+++ src/erlang_node_discovery_sup.erl
@@ -8,5 +8,8 @@ start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	Procs = [],
-	{ok, {{one_for_one, 1, 5}, Procs}}.
+    WorkersManager =  {discovery_workers_manager, {discovery_workers_manager, start_link, []}, permanent,
+                5000, worker, [discovery_workers_manager]},
+    WorkersSup  = {discovery_workers_sup, {discovery_workers_sup, start_link, []}, permanent,
+               brutal_kill, supervisor, [discovery_workers_sup]},
+	{ok, {{one_for_one, 1, 5}, [WorkersManager, WorkersSup]}}.
